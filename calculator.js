d18eb99ccd5985bae0f7a39a96e064f5a6522b58
Boakai <boakaidkamara@gmail.com>
2022-06-24 01:41:40 +0000

    validating numbers and operators while getting results

--- calculator.js
+++ calculator.js
@@ -1,24 +1,45 @@
-function calculate(first_num, second_num, operator) {
+function calculate(first_num, operator, second_num) {
 
     //validate first number
     if (isNaN(first_num)) {
         console.log("First number invalid, please input a valid number.")
+        return;
     } else if (isNaN(second_num)) {
-        console.log("Second number invalid, please input a valid number.")
+        console.log("Second number invalid, please input a valid number.");
+        return;
     }
 
-    // operators that the calculator can accept 
+    // // operators that the calculator can accept 
     let operators = ["+", "-", "*", "/"];
+    let result;
+
+
     //validate operator that a user selected
-    operators.forEach((operator) => {
-        console.log(operator)
+    let user_operator = operator;
+    if (operators.includes(user_operator) === false) {
+        console.log("invalid oprator");
+        return;
+    } else {
+        console.log("calculaing")
 
-    })
+        if (operator === "+") {
+            result = first_num + second_num;
+            // console.log(result);
+            // return result;
+        } else if (operator == "-") {
+            result = first_num - second_num;
+        } else if (operator == "*") {
+            result = first_num * second_num;
+        } else if (operator == "/") {
+            result = first_num / second_num;
+        }
 
-    let result = first_num + second_num;
-    console.log(result);
-    return result;
+        return result;
+
+    }
+
+
 }
 
 
-calculate(4, 5);
+console.log(calculate(4, "*", 4));
